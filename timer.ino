82bd99e815faa71938a3a5e76e65a750003c2203
anonymous32 <shreyas.ca@gmail.com>
2020-01-11 12:51:54 -0800

    Update timer.ino

--- timer.ino
+++ timer.ino
@@ -1,6 +1,3 @@
-// Demo the quad alphanumeric display LED backpack kit
-// scrolls through every character, then scrolls Serial
-// input onto the display
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -9,38 +6,54 @@
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
 
 void setup() {
+  Serial.begin(9600);
+  
   alpha4.begin(0x70);  // pass in the address
+  
+  alpha4.clear();
+  alpha4.writeDisplay();
+
 }
 
 
 char displaybuffer[4] = {' ', ' ', ' ', ' '};
 
 void loop() {
-  const char* foo = "0123456789";
+  char* str = "0123456789";
+
+
+  int INPUT_0 = 0;
+  int INPUT_1 = 0;
+  int INPUT_2 = 3;
+  int INPUT_3 = 0;
+
   int i = INPUT_0;
   int j = INPUT_1;
   int k = INPUT_2;
   int m = INPUT_3;
-  for(;i > 0;i--) {
-    for(;j > 0;j--) {
-      int k = INPUT_2;
-      for(;k > 0;k--) {
-        int m = INPUT_3;
-        for(;m > 0;m--) {
-        // set every digit to the buffer
-          alpha4.writeDigitAscii(0, foo[i]);
-          alpha4.writeDigitAscii(1, foo[j]);
-          alpha4.writeDigitAscii(2, foo[k]);
-          alpha4.writeDigitAscii(3, foo[m]);
+
+  for(;i >= 0;i--) {
+    for(;j >= 0;j--) {
+      for(;k >= 0;k--) {
+        for(;m >= 0;m--) {
+          alpha4.writeDigitAscii(0, str[i]);
+          alpha4.writeDigitAscii(1, str[j]);
+          alpha4.writeDigitAscii(2, str[k]);
+          alpha4.writeDigitAscii(3, str[m]);
           alpha4.writeDisplay();
           delay(1000);
+          alpha4.clear();
         }
         if(k != 0) m = 9;
       }
-      if(j != 0) k = 5;
+      if(j != 0) k = 6;
     }
     if(i != 0) j = 9;
   }
-  
- 
+
+  alpha4.writeDigitAscii(1, 'E');
+  alpha4.writeDigitAscii(2, 'n');
+  alpha4.writeDigitAscii(3, 'd');
+  alpha4.writeDisplay();
+  delay(5000);
 }
